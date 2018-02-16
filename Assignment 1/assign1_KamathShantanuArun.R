8a208b19b87aced8b2a285de503529d2a56a41d3
shantanukamath <shantanukamath@live.in>
2018-02-16 18:45:40 +0800

    Commented analysis for problem 1 till linearity

--- Assignment 1/assign1_KamathShantanuArun.R
+++ Assignment 1/assign1_KamathShantanuArun.R
@@ -1,43 +1,58 @@
-# =======================================================
+# =========================================================
 # CZ 4073 - Data Science For Business
 # Assignment 1
 # Name: KAMATH SHANTANU ARUN
 # Matric Card: U1422577F
-# =======================================================
+# =========================================================
 
 # Problem 1:
 # In this problem, our dataset is -- assign1_WineData.csv
-
+# The solution of the problem looks to fit an optimal linear
+# regression model to predict the "Quality" of the wine
 
 # ---------------------------------------------------------
-# Importing the dataset Week04wineData.csv
+# Importing the dataset Week04wineData.csv as wineData
 wineData <- read.csv("assign1_WineData.csv", header = TRUE)
-
 
 # ---------------------------------------------------------
 # Performing some basic exploratory data analysis
 
-# Dimension of the dataset
+# Dimensional information of the dataset
 dim(wineData)
+# Here we find that we have information of 1000 instances 
+# of wine and there are 12 different variables that
+# describe it
 
 # Labels of the variables in the dataset
 names(wineData)
+# It provides us the names of each of the 12 variables
 
 # Structure of the dataset
 str(wineData)
+# We find that the data is stored in a "data.frame"
+# All of the variable have numeric data type
 
 # First few rows of the data
 head(wineData)
-
 # Last few rows of the data
 tail(wineData)
+# heads and tails provide a general idea in terms of 
+# the variables actual values and their magnitudes
 
 # Summary statistics for all variables
 summary(wineData)
+# It returns a table with the results of the summary
+# function applied to each column in the data frame
+
+# For each column, it returns the minimum value, 1st quantile, 
+# median, mean, 3rd quantile and the maximum value
+# Providing us with the spread and distribution of the variables
 
 # ---------------------------------------------------------
-# Viewing plots of individual variables and checking 
-# correspondence with summary statistics
+# Visulisation of the dataset
+
+# Viewing histograms of individual variables to understand 
+# its distribution shape, center, range and variation
 hist(wineData$FixedAcidity, col = "lightgreen")
 hist(wineData$VolatileAcidity, col = "lightgreen")
 hist(wineData$CitricAcid, col = "lightgreen")
@@ -50,6 +65,8 @@ hist(wineData$pH, col = "lightgreen")
 hist(wineData$Sulphates, col = "lightgreen")
 hist(wineData$Alcohol, col = "lightgreen")
 hist(wineData$Quality, col = "lightgreen")
+# Most of the plots show a normal distribution while others
+# show right skewed distribution
 
 # Boxplots of all individual variables
 # checking correspondence with summary statistics
@@ -65,20 +82,54 @@ boxplot(wineData$pH, horizontal = TRUE, col = "steelblue")
 boxplot(wineData$Sulphates, horizontal = TRUE, col = "steelblue")
 boxplot(wineData$Alcohol, horizontal = TRUE, col = "steelblue")
 boxplot(wineData$Quality, horizontal = TRUE, col = "steelblue")
+# Box plots were able to clearly point out potential outliers 
+# and provide a better sense on where the median or mean lie
+# hence improving our understanding of the frequency histograms
+# and the entire dataset
+
 
 # ---------------------------------------------------------
-# Checking correlations between variables
+# Checking correlation between the variable of the dataset
 cor(wineData)
 # install.packages("corrplot")
 library(corrplot)
 corrplot.mixed(cor(wineData))
+# corrplot library uses colours and shape to visual represent
+# the strength of the correlations in a matrix format
+
+# With the correlation matrix, we find that "Quality" doesnot 
+# have a very strong correlation with any of the variables.
+# The most correlation being with Alcohol (0.47) and 
+# VolatileAcidity (-0.39)
+# Low or zero correlated variable will be least significant 
+# and would be eliminated early to create the best model
+
+# The highest correlation(|0.66-0.67|) found was betwen 
+# FixedAcidity & Density,
+# FixedAcidity & pH,
+# FixedAcidity & CitricAcid
+# FreeSulphurDioxide & TotalSulphurDioxide,
+# Hence these variables are likely to absorb each others
+# contribution to Quality and may cause them to be futile
+# in predicting Quality
 
 # ---------------------------------------------------------
-# Plot 2d scatterplots of all pairs of variables
+# Plotting two-dimensional scatterplots of all pairs of variables
 pairs(wineData, pch = 19, col = "blue")
+# Viewing the scatter plots we can understand why the correlation
+# of the various variable pairs are either high or low
+# It is difficult to judge the plots that involve Quality because
+# its type is integer and more discrete than continuous.
+
+# The scatter plot between sulphate and Quality seem to be less 
+# spherical and hence point towards some linearity/nonlinearity
+# which would be a good place to do some trial and error.
 
 # ---------------------------------------------------------
 # Perform Linear Regression (Quality vs rest of the variables)
+
+# We will use adjusted R-squared as the metric to determine
+# if the model is improving
 
 # Linear model on Quality vs Rest of the variables
 # Model 1. - Full Model
@@ -87,9 +138,23 @@ lmFit1 <- lm(Quality ~ ., data = wineData)
 # Display the summary of Model 1.
 summary(lmFit1)
 
+# The "Full Model" summary statistics provide us with information
+# on the formula, residuals, and coefficients.
+# The coefficients provide a scale of significance on each of
+# the variables that provide a contribution on predicting the Quality.
+
 # ---------------------------------------------------------
 # Building an Updated Model by removing the least significant 
 # variable at a time
+
+# To do so, we would want to eliminate the most insignificant
+# variables having "." or lesser on the scale.
+# With variables having the same significance, we will use 
+# Probability (Pr) to decide
+
+# The value in the Pr colum informs us of the probability of 
+# coefficient of the variable to go to zero. The higher the 
+# probability the lower its significance.
 
 # Linear Model on Quality vs Rest minus FixedAcidity
 # Model 2.
@@ -98,12 +163,19 @@ lmFit2 <- update(lmFit1, ~ . - FixedAcidity, data = wineData)
 # Display the summary of the updated linear model
 summary(lmFit2)
 
+# In Model 2,  we removed the variable FixedAcidity (Pr = 0.76) 
+# as it was the least significant
+# This helped improve the R-squared to 0.3413 from 0.3407
+
 # Linear Model on Quality vs Rest minus FixedAcidity, CitricAcid
 # Model 3.
 lmFit3 <- update(lmFit2, ~ . - CitricAcid, data = wineData)
 
 # Display the summary of the updated linear model
 summary(lmFit3)
+
+# Similarly in Model 3, we removed the variable CitricAcid (Pr = 0.75) 
+# This helped improve the R-squared to 0.3419 from 0.3413
 
 # Linear Model on Quality vs Rest minus FixedAcidity, CitricAcid,
 # ResidualSugar
@@ -113,6 +185,9 @@ lmFit4 <- update(lmFit3, ~ . - ResidualSugar, data = wineData)
 # Display the summary of the updated linear model
 summary(lmFit4)
 
+# Similarly in Model 4, we removed the variable ResidualSugar (Pr = 0.33) 
+# This helped improve the R-squared to 0.342 from 0.3419
+
 # Linear Model on Quality vs Rest minus FixedAcidity, CitricAcid,
 # ResidualSugar, Density
 # Model 5.
@@ -120,6 +195,13 @@ lmFit5 <- update(lmFit4, ~ . - Density, data = wineData)
 
 # Display the summary of the updated linear model
 summary(lmFit5)
+
+# Lastly in Model 5, we removed the variable Density (Pr = 0.48) 
+# This helped improve the R-squared to 0.3423 from 0.3420
+
+# Now we have covered the linearity part of the model 
+# and kept only the variables that have a significance of 
+# Pr lower than 0.05 (*).
 
 # ---------------------------------------------------------
 # Checking for Non-linear Relations with Variables
