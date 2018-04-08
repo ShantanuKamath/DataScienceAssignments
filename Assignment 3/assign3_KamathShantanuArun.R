056159305a8f433477214ce4426ee0f0922d39fe
shantanukamath <shantanukamath@live.in>
2018-04-08 22:28:12 +0800

    Completed first part in final submission draft

--- Assignment 3/assign3_KamathShantanuArun.R
+++ Assignment 3/assign3_KamathShantanuArun.R
@@ -135,13 +135,20 @@ rect.hclust(hiercFit, k = K)
 
 # -------------------------------------------------------
 # Perform K-Means Clustering with K determined using above 
-# Dendogram
+# Dendogram. K means tends to form similar shaped clusters.
+# Hence even if there were actually 2 clusters but the shapes, 
+# differ in size, it is possible that K-means was not able
+# to optimally cluster the dataset. They also could be faulting
+# because they look to perform Hard Spherical Clustering, while,
+# the data itself may not be organised like that.
 K <- 2
 kMeansFit <- kmeans(cuisDF, centers = K, nstart = 20)
 kMeansFit
 
 # -------------------------------------------------------
-# Finding the "optimal" value of K for K-Means
+# Finding the optimal K value for clustering via K-Means.
+# Runing through different values of K, and visualize where 
+# the optimal value could lie. 
 kMin <- 1
 kMax <- 20
 withinSS <- double(kMax - kMin + 1)
@@ -163,57 +170,97 @@ plot(kMin:kMax, betweenSS, pch=19, type="b", col="green",
        xlab = "Value of K", ylab = "Sum of Squares (Within and Between)")
 
 # The only elbow point that could be visualized from the
-# two plots is at K = 2. 
-
+# two plots is at K = 2.
 
 # -------------------------------------------------------
-# Task 7 : Perform EM Clustering on the dataset
+# Perform EM Clustering on the dataset
 # install.packages("mclust")
 library(mclust)
 emFit <- Mclust(cuisDF)
 summary(emFit)
+# EM Clustering does not support the assumption that the 
+# optimal number of clusters is 2. 
+# This maybe due to the shape of the clusters being formed.
+# EM tends to find elliptical or gaussian clusters and 
+# hence could divide  long oddly shaped or chain clusters
+# into multiple smaller clusters.
 
-plot(emFit, what = "classification")
-emFit$parameters
+# -------------------------------------------------------
+# Upon performing K-Means and EM Clustering we could probably
+# conclude that the structure of the data is arbitrary.
+# Hierarchical clustering works well in such situations. 
 
 # -------------------------------------------------------
 # Optimizing through variations in Hierarchical Clustering
-hiercFit <- hclust(dist(cuisDF, method = "euclidean"),
-                   method="single")
+
+# -------------------------------------------------------
+# Single 
+hiercFit <- hclust(dist(cuisDF, method = "euclidean"),method="single")
 hiercFit
+
 # Visualize Dendogram
 plot(hiercFit, main="Minimum Variance", xlab="", ylab="", sub="", cex =.5)
+# Single method of clustering tends to produce long 
+# thin cluster chains.With the dendogram we can see
+# that there is no easy distinction to form or clearly
+# gain the optimal number of clusters. This type of 
+# method doesn't seem to suit the structure of the data.
 
-
-
+# -------------------------------------------------------
+# Complete 
 hiercFit <- hclust(dist(cuisDF, method = "euclidean"),
                    method="complete")
 hiercFit
+
 # Visualize Dendogram
 plot(hiercFit, main="Minimum Variance", xlab="", ylab="", sub="", cex =.5)
+# Complete method excels in identifying compact clusters 
+# of similar diameters. However the dendogram doesnt show
+# clear seperation of clusters between each other. The data
+# is not suited for complete linkage. We can even see that 
+# some single points are added to the clusters at the end.
+# Signalling bad clustering done before hand.
 
-
-
-hiercFit <- hclust(dist(cuisDF, method = "euclidean"),
-                   method="average")
+# -------------------------------------------------------
+# Average 
+hiercFit <- hclust(dist(cuisDF, method = "euclidean"), method="average")
 hiercFit
 # Visualize Dendogram
 plot(hiercFit, main="Minimum Variance", xlab="", ylab="", sub="", cex =.5)
+# Average, basically averages the pairwise distances dist(x,y).
+# Averages seems to work differently than other methods in the ways it
+# is combining the clusters in the dendogram. 
+# We find that there is no proper straight cut that could be used to determine
+# optimal clusters.
 
+# -------------------------------------------------------
+# The initial model built turned out to be the best suited
+# for the dataset. It seems to provide the arbitrary shape of the 
+# the two cluster with clearly defined seperation.
 
+# Ward 
+hiercFit <- hclust(dist(cuisDF, method = "euclidean"),method="ward.D")
+hiercFit
 
+# Visualize Dendogram
+plot(hiercFit, main="Minimum Variance", xlab="", ylab="", sub="", cex =.5)
 
+# Number of clusters : 2 
+K <- 2 
+rect.hclust(hiercFit, k = K)
 
+# =========================================================
+# Problem 2:
+# In this problem, our dataset is -- responses.csv. It 
+# documents the responses from a survey, connecting individual 
+# preferences to the demography. The target is to perform 
+# clustering with appropriate choice of algorithm and distance
+# notion to identify the optimal number of clusters in the dataset.
+# Also mention the strongest parameters and answer if you think
+# Gender plays a major role in clustering
 
-
-
-
-
-
-
-
+# =======================================================
 
 # ---------------------------------------------------------
 # Clean environment and remove all working data
 rm(list = ls())
-
